db1eea40bf962b0c0458a6ea110c34bf716c056b
celeste-vidotto <celesvidotto@gmail.com>
2026-05-05 21:33:36 -0300

    finalizo cp15 y cp16

--- OrangeHMR-tests/tests_sidebar/tests-modulo-admin/CP15-queryUserNotExist.spec.js
+++ OrangeHMR-tests/tests_sidebar/tests-modulo-admin/CP15-queryUserNotExist.spec.js
@@ -1,53 +1,36 @@
 import { test, expect } from '@playwright/test';
+import { LoginHelp } from '../../HELPER/LoginHelp.js';
+import { adminPage } from '../../MAPEOSpage/adminPage.js';
+const user = require('../../DATOS/users.json'); 
 
-test('test', async ({ page }) => {
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await expect(page.getByRole('button', { name: 'Search' })).toBeVisible();
+test('Query user not exist', async ({ page }) => { 
+
+//arrange
+    await LoginHelp(page, user.usuarioValido.username, user.usuarioValido.password);
+    const camposDeFiltro = {
+        username: user.filtroUserInvalido.username,
+        userRole: user.filtroUserInvalido.userRole,
+        employeeName: user.filtroUserInvalido.employeeName,
+        status: user.filtroUserInvalido.status
+    };
+    const OpenAdmin = new adminPage(page); //instancio la clase adminPage para usar sus metodos
+    await OpenAdmin.OpenModuloAdmin(); //uso el metodo para abrir el modulo admin
+    await OpenAdmin.AdminPageLoaged('Admin'); //verifico que el modulo admin se haya cargado
+    await OpenAdmin.AdminElementsBeVisible(); //verifico que los elementos principales del modulo admin sean visibles
+ 
+
+//act
+  await page.getByRole('textbox').nth(1).fill(camposDeFiltro.username);  
+  await OpenAdmin.userRole(camposDeFiltro.userRole); 
+  //deberia sumar el employee name "invalidate"
+  await OpenAdmin.Status(camposDeFiltro.status); 
   await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByRole('textbox').nth(1).click();
-  await page.locator('div').filter({ hasText: /^-- Select --$/ }).nth(2).click();
-  await page.getByRole('textbox', { name: 'Type for hints...' }).click();
-  await page.getByText('-- Select --').nth(1).click();
-  await page.getByText('-- Select --').nth(1).click();
-  await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByText('(5) Records Found').click();
-  await page.getByText('(5) Records Found').click();
 
 
+  //asserts
+  await expect(page.locator('#oxd-toaster_1').getByText('No Records Found')).toBeVisible();
+  await expect(page.locator('span').filter({ hasText: 'No Records Found' })).toBeVisible();
+ 
 
-
-  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-  await expect(page.getByRole('button', { name: 'Search' })).toBeVisible();
-  await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByRole('textbox').nth(1).click();
-  await page.locator('div').filter({ hasText: /^-- Select --$/ }).nth(2).click();
-  await page.getByRole('textbox', { name: 'Type for hints...' }).click();
-  await page.getByText('-- Select --').nth(1).click();
-  await page.getByText('-- Select --').nth(1).click();
-  await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByText('(5) Records Found').click();
-  await page.getByText('(5) Records Found').click();
-  await expect(page.getByRole('textbox').nth(1)).toBeVisible();
-  await page.getByRole('textbox').nth(1).click();
-  await page.getByRole('textbox').nth(1).fill('userinvent');
-  await page.locator('.oxd-icon.bi-caret-down-fill.oxd-select-text--arrow').first().click();
-  await page.getByRole('option', { name: 'Admin' }).click();
-  await page.getByRole('textbox', { name: 'Type for hints...' }).click();
-  await page.getByRole('textbox', { name: 'Type for hints...' }).fill('layer');
-  await page.getByRole('option', { name: 'No Records Found' }).click();
-  await page.getByText('Invalid').click();
-  await page.locator('.oxd-autocomplete-text-input').click();
-  await page.locator('div').filter({ hasText: /^-- Select --$/ }).nth(2).click();
-  await page.getByRole('listbox').getByText('Enabled').click();
-  await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByRole('button', { name: 'Search' }).click();
-  await page.locator('div').filter({ hasText: 'System UsersUsernameUser' }).nth(4).click();
-  await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByRole('textbox', { name: 'Type for hints...' }).click();
-  await page.getByRole('textbox', { name: 'Type for hints...' }).fill('muser');
-  await page.getByRole('option', { name: 'muser 4957589' }).click();
-  await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByRole('button', { name: 'Search' }).click();
-  await page.locator('span').filter({ hasText: 'No Records Found' }).click();
 });
+
--- OrangeHMR-tests/tests_sidebar/tests-modulo-admin/CP16-emptyQuery.spec.js
+++ OrangeHMR-tests/tests_sidebar/tests-modulo-admin/CP16-emptyQuery.spec.js
@@ -1,8 +1,34 @@
-  await page.getByRole('textbox').nth(1).click();
-  await page.getByText('-- Select --').first().click();
-  await page.getByRole('option', { name: '-- Select --' }).click();
-  await page.getByRole('textbox', { name: 'Type for hints...' }).click();
-  await page.locator('div:nth-child(4) > .oxd-input-group > div:nth-child(2) > .oxd-select-wrapper > .oxd-select-text').click();
-  await page.getByRole('option', { name: '-- Select --' }).click();
+import { test, expect } from '@playwright/test';
+import { LoginHelp } from '../../HELPER/LoginHelp.js';
+import { adminPage } from '../../MAPEOSpage/adminPage.js';
+const user = require('../../DATOS/users.json'); 
+
+test('Query user not exist', async ({ page }) => { 
+
+//arrange
+    await LoginHelp(page, user.usuarioValido.username, user.usuarioValido.password);
+    const camposDeFiltro = {
+        username: user.filtroUserEmpty.username,
+        userRole: user.filtroUserEmpty.userRole,
+        employeeName: user.filtroUserEmpty.employeeName,
+        status: user.filtroUserEmpty.status
+    };
+    const OpenAdmin = new adminPage(page); //instancio la clase adminPage para usar sus metodos
+    await OpenAdmin.OpenModuloAdmin(); //uso el metodo para abrir el modulo admin
+    await OpenAdmin.AdminPageLoaged('Admin'); //verifico que el modulo admin se haya cargado
+    await OpenAdmin.AdminElementsBeVisible(); //verifico que los elementos principales del modulo admin sean visibles
+ 
+
+//act
+  await page.getByRole('textbox').nth(1).fill(camposDeFiltro.username);  
+  await OpenAdmin.userRole(camposDeFiltro.userRole); 
+  await OpenAdmin.Status(camposDeFiltro.status); 
   await page.getByRole('button', { name: 'Search' }).click();
-  await page.getByText('(5) Records Found').click();
+
+
+  //asserts
+  await expect(page.getByText('No Records Found')).toBeVisible(); 
+  await expect(page.getByText('Error Invalid Parameter')).toBeVisible();
+ 
+
+});
